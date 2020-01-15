72d1ceaa715206c5b30c851928692b9b8fedb433
Naveed Aheer <naveedaheer@gmail.com>
2020-01-15 11:33:07 -0500

    update cart style

--- src/Components/products/cartItems.js
+++ src/Components/products/cartItems.js
@@ -13,12 +13,15 @@ export default class CartItems extends Component {
             })
             console.log("state", this.state)
     }
-  
+    
+    handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     render() {
-      const message = `The current state is ${this.state.items[0].title}.`;
       return (
           <div className="container white-text text-darken-3">
-                <h5 className="white-text text-darken-3">Products in your cart</h5>
+                <h5 className="white-text text-darken-3">Products in your Cart</h5>
         <div>
         <ol type="1">
             {
@@ -28,7 +31,21 @@ export default class CartItems extends Component {
                 )
             }
 </ol>
-<button>Checkout</button>
+<div>
+<form onSubmit ={this.handleSubmit} >
+                <div className="input-field">
+                <label className= "white-text" htmlFor="title">Name</label>
+                <input className= "white-text" type="text" id="title" onChange={this.handleChange} />
+                </div>
+                <div className="input-field">
+                <label className= "white-text"htmlFor="content">Order Details(Optional)</label>
+                <textarea  id="content" className="white-text materialize-textarea" onChange={this.handleChange}></textarea>
+                </div>
+                <div className="input-field">
+                    <button className="btn pink lighten-1 z-depth-0">Checkout</button>
+                </div>
+            </form>
+</div>
         </div>
           </div>
       );
