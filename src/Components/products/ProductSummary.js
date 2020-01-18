26d9bb14c0b2e5b177e2918858a5619e04255292
Naveed Aheer <naveedaheer@gmail.com>
2020-01-17 20:21:03 -0500

    Implemented image from db

--- src/Components/products/ProductSummary.js
+++ src/Components/products/ProductSummary.js
@@ -2,14 +2,18 @@ import React from 'react'
 import {connect} from 'react-redux'
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import productImage from "../../images/product/product1.jpg"
 //import ProductDetails from './ProductDetails'
 
 const ProductSummary = ({product}) =>{
-
-
+console.log("pro", product);
     return(
         <div className="product" >
-          <div className="product-image">
+          <div>
+          {
+product.imageURL ? 
+          <img className="product-image" src={product.imageURL} /> : <img className="product-image" src={productImage} />
+          }  
           </div>
           <p className="product-title"> {product.title} </p>
         </div>      
